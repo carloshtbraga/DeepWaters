7164fabcae7e402638e197f509dbbf979f5e8924
carloshtbraga <carloshtbraga@gmail.com>
2023-07-29 17:26:47 -0300

    refactor

---
+++ src/components/Sky.jsx
@@ -0,0 +1,10 @@
+export default function Sky() {
+  return (
+    <div className="ceu">
+        <div className="estrela"></div>
+        <div className="estrela"></div>
+        <div className="estrela"></div>
+        <div className="estrela"></div>
+      </div>
+  )
+}
