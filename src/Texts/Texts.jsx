227466cd41bc13a028cfd0f550912c8e8fcd8cea
carloshtbraga <carloshtbraga@gmail.com>
2023-08-01 12:44:09 -0300

    oi

---
+++ src/Texts/Texts.jsx
@@ -0,0 +1,23 @@
+export const text1pt = "Você queria mudar sua vida... Agora você está no meio do oceano. Você não tem certeza do que está fazendo aqui. Tudo o que sabe é que deixou algo para trás e agora está em busca de algo novo. Dúvidas surgem em sua mente, questionando se você está realmente pronto para esse caminho desconhecido. Apesar da incerteza, algo te impulsiona para frente, e você escolhe abraçar a aventura que está à sua frente. Você continua seguindo em frente."
+
+export const text1en = "You wanted to change your life... You are now in the middle of the ocean. You are not sure what you are doing here. All you know is you left something behind, and you are now looking for something new. Doubts creep into your mind, questioning whether you are truly ready for this unknown path. Despite the uncertainty, something pushes you forward, and you choose to embrace the adventure that lies ahead. You keep going."
+
+export const text2pt ="Você está prestes a entrar em águas profundas. Esqueça tudo o que você sabe... as coisas vão funcionar de forma diferente a partir de agora. Mas talvez seja isso que você realmente queria. Então, você continua seguindo em frente."
+
+export const text2en = "You are about to get into deep waters. Forget everything you know... things will work differently from now on. But maybe, thats what you really wanted. So you keep going."
+
+export const text3pt ="As criaturas que você enfrenta agora são maiores e mais fortes, mas elas caem uma por uma. Você olha para baixo e percebe que mal consegue ver as coisas. Você começa a ficar realmente assustado, quando isso vai acabar? Mas você continua seguindo em frente."
+
+export const text3en = "The creatures you face now are bigger and stronger, but they fall one by one. You look down and realize you can barely see things. You start to get really scared, when will this end? But you keep going."
+
+export const text4pt ="Agora está frio, escuro, solitário, silencioso e a pressão é inimaginável. Este não é um lugar para humanos. De alguma forma, você se sente confortável. À medida que você se aprofunda, vê menos criaturas, mas elas ficam cada vez mais estranhas. Você sabe que agora não há mais volta nesta jornada, então... você continua seguindo em frente."
+
+export const text4en = "It's now cold, dark, lonely, silent, and the pressure is unimaginable. This is not a place for humans at all. Somehow you feel comfortable. As you go deeper, you see fewer creatures, but they get weirder and weirder. You know that now, there's no way back on this journey, so... you keep going."
+
+export const text5pt = "Agora você está na parte mais profunda do oceano. Isso é chamado de zona Abissal ou O Abismo. A escuridão reina. Essa região nunca teve luz. É o lugar mais escuro da Terra. Isso só é comparável ao espaço. Você se sente fraco e cansado. Você está prestes a desistir. Então você pensa em todas as coisas que aprendeu e o quanto cresceu. Você pensa em todas as pessoas que dependem de você. Além disso, você está inexplicavelmente aproveitando a jornada. Então você continua seguindo em frente."
+
+export const text5en = "You are now in the deepest part of the ocean. This is called the Abyssal zone or The Abyss. Darkness reigns. This region has actually never had light. It is the darkest place on Earth. This is only comparable to space. You feel weak and tired. You are about to give up. Then you think of all the things you have learned and how much you have grown. You think of all the people that are depending on you. Also, you are unreasonably enjoying the journey. So you keep going."
+
+export const text6pt = "No fundo do oceano, você ouve algo. Olha ao redor e não vê nada, mas sente calor se aproximando. Isso acende esperança dentro de você, e você nada em direção a isso. Você vê muitas pessoas lá. Todas são desenvolvedoras (devs). Elas estão todas felizes e satisfeitas, tendo abraçado os desafios do abismo. Enquanto eles te recebem de braços abertos, você percebe que não está destinado a enfrentar essa jornada sozinho. Por que você não se junta a eles?"
+
+export const text6en = "At the bottom of the ocean, you hear something. You look around and see nothing, but you feel warmth coming. It sparks hope within you, and you swim towards it. You see a lot of people there. They are all developers (devs). They are all happy and content, having embraced the challenges of the abyss. As they welcome you with open arms, you realize that you are not meant to face this journey alone. Why don't you join them?"
--- src/components/Ocean.jsx
+++ src/components/Ocean.jsx
@@ -12,11 +12,34 @@ import passador6 from "../assets/passador6.gif";
 import passador7 from "../assets/passador7.gif";
 import passador9 from "../assets/passador9.gif";
 import estranho2 from "../assets/estranho2.gif";
+import eua from "../assets/uk.png";
+import brasil from "../assets/br.png";
 
 import passador10 from "../assets/passador10.gif";
+import { useState } from "react";
 
+import {
+  text1en,
+  text1pt,
+  text2en,
+  text2pt,
+  text3en,
+  text3pt,
+  text4en,
+  text4pt,
+  text5en,
+  text5pt,
+  text6en,
+  text6pt,
+} from "../Texts/Texts.jsx";
 
 export default function Ocean() {
+  const [en, setEn] = useState(true);
+
+  const languageToggle = () => {
+    setEn(!en);
+  };
+
   return (
     <>
       <img src={wave} alt="" className="wave" />
@@ -31,12 +54,15 @@ export default function Ocean() {
             <div className="divimg">
               <img src={gif1} alt="" className="gif1" />
             </div>
-            You wanted to change your life... You are now in the middle of the
-            ocean. You are not sure if you are ready for this journey. Doubts
-            creep into your mind, questioning whether you are truly ready for
-            this unknown path. Despite the uncertainty, something pushes you
-            forward, and you choose to embrace the adventure that lies ahead.
-            You keep going.
+            <button className="buttonen" onClick={languageToggle}>
+              {en ? (
+                <img className="bandeira" src={brasil} alt="" />
+              ) : (
+                (<img className="bandeira" src={eua} alt="" />)
+              )}
+            </button>
+            <br />
+            {en ? text1en : text1pt}
           </h1>
         </div>
 
@@ -45,10 +71,16 @@ export default function Ocean() {
             <div className="divimg">
               <img src={gif2} alt="" className="gif1" />
             </div>
-       
-            You are about to get into deep waters. Forget everything you know...
-            things will work differently from now on. But maybe,thats what you
-            really wanted. So you keep going.
+
+            <button className="buttonen" onClick={languageToggle}>
+              {en ? (
+                <img className="bandeira" src={brasil} alt="" />
+              ) : (
+                (<img className="bandeira" src={eua} alt="" />)
+              )}
+            </button>
+            <br />
+            {en ? text2en : text2pt}
           </h1>
         </div>
 
@@ -57,56 +89,63 @@ export default function Ocean() {
             <div className="divimg">
               <img src={passador9} alt="" className="gif1" />
             </div>
-            The creatures you face are now bigger and stronger, yet they fall
-            one by one. You look down and you notice that you cant barely see
-            things anymore. You start to get really scared, when will this end?
-            But you keep going.
+            <button className="buttonen" onClick={languageToggle}>
+              {en ? (
+                <img className="bandeira" src={brasil} alt="" />
+              ) : (
+                (<img className="bandeira" src={eua} alt="" />)
+              )}
+            </button>
+            <br />
+            {en ? text3en : text3pt}
           </h1>
         </div>
-
 
         <div className="divh1 animate__animated animate__fadeIn">
           <h1 className="deepwaters ">
             <div className="divimg">
               <img src={gif3} alt="" className="gif3" />
             </div>
-            Its now cold, dark, lonely, silent and the pressure is unimaginable.
-            This is not a place for humans at all. Somehow you feel confortable.
-            As you go deeper, you see less creatures but they get weirder and
-            weirder. You know that now, theres no way back on this journey so...
-            you keep going.
+            <button className="buttonen" onClick={languageToggle}>
+              {en ? (
+                <img className="bandeira" src={brasil} alt="" />
+              ) : (
+                (<img className="bandeira" src={eua} alt="" />)
+              )}
+            </button>
+            <br />
+            {en ? text4en : text4pt}
           </h1>
         </div>
 
-     
         <div className="divh1 animate__animated animate__fadeIn">
           <h1 className="deepwaters ">
             <div className="divimg">
               <img src={gif4} alt="" className="gif4" />
-             
             </div>
-            You are now in the deepest part of the ocean. This is called Abyssal
-            zone or The Abyss. Darkness reigns. This region has actually never
-            had light. It is the darkest place on Earth. This is only comparable
-            to the space. You feel weak and tired. You are about to give up.
-            Then you think of all the things you have learned and how much you
-            have grown. You think of all the people that are depending on you.
-            Also you are unreasonably enjoying the journey. So you keep going. 
+            <button className="buttonen" onClick={languageToggle}>
+              {en ? (
+                <img className="bandeira" src={brasil} alt="" />
+              ) : (
+                (<img className="bandeira" src={eua} alt="" />)
+              )}
+            </button>
+            <br />
+            {en ? text5en : text5pt}
           </h1>
         </div>
-       
 
         <div className="divh1 animate__animated animate__fadeIn">
           <h1 className="deepwaters ">
-       
-
-            At the bottom of the ocean you hear something. You look around and
-            see nothing but you feel warmth coming.It sparks hope within you and
-            you swim towards it. You see a lot of people there. They are all
-            devs. They are all happy and contentment, having embraced the
-            challenges of the abyss. As they welcome you with open arms, you
-            realize that you are not meant to face this journey alone. Why dont
-            you join them?{" "}
+          <button className="buttonen" onClick={languageToggle}>
+              {en ? (
+                <img className="bandeira" src={brasil} alt="" />
+              ) : (
+                (<img className="bandeira" src={eua} alt="" />)
+              )}
+            </button>
+            <br />
+            {en ? text6en : text6pt}
             <a
               href="https://www.devs4good.com.br"
               target="_blank"
@@ -117,7 +156,7 @@ export default function Ocean() {
             </a>
 
             <div className="divimg">
-            <img src={estranho2} alt="" className="gif5" />
+              <img src={estranho2} alt="" className="gif5" />
               {/* <img src={astro} alt=""  className="astro"/> */}
             </div>
           </h1>
